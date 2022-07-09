f7317fa8b43cb30530bf720887e4e43883492ea0
Leonardo Diniz <leonardodiber@gmail.com>
2022-07-08 23:09:06 -0300

    basic kitty config and plugin install for neovim

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,6 @@
-local colorscheme = "tokyonight"
+vim.g.gruvbox_italic = 1
+
+local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
