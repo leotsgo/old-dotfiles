df941ba799bc9d3a9d3e9c53f18bab2c76467f93
Leonardo Diniz <leonardodiber@gmail.com>
2022-07-15 01:10:53 -0300

    refactor(aliases): changes alias to edit kitty.conf from kitty.conf to kconf

--- .config/.zsh/aliases.zsh
+++ .config/.zsh/aliases.zsh
@@ -40,7 +40,7 @@ alias ls='ls --color=auto'
 alias l.='ls -d .* --color=auto'
 
 # Quick aliases to edit config files
-alias kitty.conf='vim ~/.config/kitty/kitty.conf'
+alias kconf='vim ~/.config/kitty/kitty.conf'
 alias zshrc='vim ~/.zshrc'
 alias asdfrc='vim ~/.asdfrc'
 alias awesomerc='vim ~/.config/awesome/rc.lua'
