33eb0847bcb3a7a06b601dd607b8c791dbee1f0f
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-02 18:35:37 -0300

    style(nvim): formats plugins.lua file

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -89,7 +89,7 @@ return packer.startup(function(use)
 
   -- Treesitter
   use({ "nvim-treesitter/nvim-treesitter" })
-  use("p00f/nvim-ts-rainbow")
+  use({ "p00f/nvim-ts-rainbow" })
 
   -- Git
   use({ "lewis6991/gitsigns.nvim" })
