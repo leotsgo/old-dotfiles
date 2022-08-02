bee18b5322de4478b15aac1fa5d69432c84802e0
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-01 21:41:25 -0300

    feat(nvim): enables format on save

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -7,20 +7,34 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
-null_ls.setup {
+null_ls.setup({
   debug = false,
   sources = {
-    formatting.prettier.with {
+    formatting.prettier.with({
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
+    }),
+    formatting.black.with({ extra_args = { "--fast" } }),
     -- formatting.isort,
     formatting.gofmt,
     formatting.stylua,
     diagnostics.flake8,
     diagnostics.golangci_lint,
   },
-}
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+})
