c2eec6da9e70323f9869381052d5ec8b0bebffaa
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-02 22:33:03 -0300

    feat(nvim): lazygit added to config

--- .config/nvim/lua/user/toggleterminal.lua
+++ .config/nvim/lua/user/toggleterminal.lua
@@ -8,3 +8,21 @@ toggleterm.setup({
   open_mapping = [[<c-\>]],
   size = 20,
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  size = 40,
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    winblend = 2,
+  },
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
