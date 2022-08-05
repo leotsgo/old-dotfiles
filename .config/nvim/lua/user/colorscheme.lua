30f4591db87dd3d38dec0eb787c273e077878a05
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-04 23:42:40 -0300

    feat(nvim): changes in colorscheme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,9 @@
 vim.g.gruvbox_italic = 1
+vim.o.background = "dark"
 
 local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
