0f90ef4e895747154222969b3b96955f4af793ac
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-06 23:41:48 -0300

    feat(nvim): adds lsp support for typescript/javascript

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -1,55 +1,61 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-  return
+	return
 end
 
 local servers = {
-  "sumneko_lua",
-  "jsonls",
-  "pyright",
-  "gopls",
-  "bashls",
+	"sumneko_lua",
+	"jsonls",
+	"pyright",
+	"gopls",
+	"bashls",
+	"tsserver",
 }
 
 lsp_installer.setup()
 
 local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lsp_status_ok then
-  return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+	opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
 
-  if server == "jsonls" then
-    local jsonls_opts = require("user.lsp.settings.jsonls")
-    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-  end
+	if server == "jsonls" then
+		local jsonls_opts = require("user.lsp.settings.jsonls")
+		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	end
 
-  if server == "sumneko_lua" then
-    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
+	if server == "sumneko_lua" then
+		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
 
-  if server == "pyright" then
-    local pyright_opts = require("user.lsp.settings.pyright")
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
+	if server == "pyright" then
+		local pyright_opts = require("user.lsp.settings.pyright")
+		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	end
 
-  if server == "gopls" then
-    local gopls_opts = require("user.lsp.settings.gopls")
-    opts = vim.tbl_deep_extend("force", gopls_opts, opts)
-  end
+	if server == "gopls" then
+		local gopls_opts = require("user.lsp.settings.gopls")
+		opts = vim.tbl_deep_extend("force", gopls_opts, opts)
+	end
 
-  if server == "bashls" then
-    local bashls_opts = require("user.lsp.settings.bashls")
-    opts = vim.tbl_deep_extend("force", bashls_opts, opts)
-  end
+	if server == "bashls" then
+		local bashls_opts = require("user.lsp.settings.bashls")
+		opts = vim.tbl_deep_extend("force", bashls_opts, opts)
+	end
 
-  lspconfig[server].setup(opts)
+	if server == "tsserver" then
+		local bashls_opts = require("user.lsp.settings.tsserver")
+		opts = vim.tbl_deep_extend("force", bashls_opts, opts)
+	end
+
+	lspconfig[server].setup(opts)
 end
--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -12,29 +12,30 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.prettier.with({
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    }),
-    formatting.black.with({ extra_args = { "--fast" } }),
-    -- formatting.isort,
-    formatting.gofmt,
-    formatting.stylua,
-    diagnostics.flake8,
-    diagnostics.golangci_lint,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_filetypes = { "toml" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.isort,
+		formatting.gofmt,
+		formatting.stylua,
+		diagnostics.flake8,
+		diagnostics.golangci_lint,
+		diagnostics.eslint,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
