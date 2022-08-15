bb20714de34e47b4176dd80b02475bb3b0f7daad
Leonardo Diniz <leonardodiber@gmail.com>
2022-08-15 02:21:19 -0300

    feat(nvim): adding a bunch of uncomitted stuff

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -31,7 +31,7 @@ local options = {
 	showcmd = false,
 	ruler = false,
 	numberwidth = 4, -- set number column width to 2 {default 4}
-	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+	signcolumn = "auto:1", -- always show the sign column, otherwise it would shift the text each time
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
@@ -39,7 +39,6 @@ local options = {
 }
 
 vim.opt.shortmess:append("c")
-vim.opt.fillchars.eob = " "
 vim.opt.whichwrap:append("<,>,[,],h,l")
 vim.opt.iskeyword:append("-")
 vim.g.leetcode_solution_filetype = "golang"
---
+++ .config/nvim/lua/user/true-zen.lua
@@ -0,0 +1,6 @@
+local status_ok, truezen = pcall(require, "truezen")
+if not status_ok then
+	return
+end
+
+truezen.setup()
