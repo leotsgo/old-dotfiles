b69987c282605599969a0d97539e4d2769e68050
Leonardo Diniz <leonardodiber@gmail.com>
2022-12-10 02:49:18 -0300

    feat(nvim): removing unused files after chaning lsp plugin to lsp-zero

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -4,5 +4,4 @@ if not status_ok then
 end
 
 require("user.lsp.lsp-zero")
-require("user.lsp.handlers").setup()
 require("user.lsp.null-ls")
--- .config/nvim/lua/user/lsp/lsp-zero.lua
+++ .config/nvim/lua/user/lsp/lsp-zero.lua
@@ -1,5 +1,10 @@
 local lsp = require("lsp-zero")
-
+local keymap = vim.keymap.set
+local key_opts = {}
 lsp.preset("recommended")
+local rust_lsp = lsp.build_options("rust_analyzer", {})
 lsp.nvim_workspace()
 lsp.setup()
+
+-- Initialize rust_analyzer with rust-tools
+require("rust-tools").setup({ server = rust_lsp })
