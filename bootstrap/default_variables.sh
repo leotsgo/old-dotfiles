3c40715224da71483b0e96cfdc93e8d0996dc170
Leonardo Diniz <leonardodiber@gmail.com>
2023-01-28 01:16:15 -0300

    feat: creates file with default environment variables for scripts

---
+++ bootstrap/default_variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+HOME_OS="Arch Linux"
+WORK_OS="Ubuntu"
+WORK_EMAIL="leonardo.diniz@dito.com.br"
+PERSONAL_EMAIL="leonardodiber@gmail.com"
--- bootstrap/generate_gitconfig.sh
+++ bootstrap/generate_gitconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+. ./default_variables.sh
 git config --global user.name "Leonardo Diniz"
 git config --global push.autoSetupRemote true
 git config --global alias.s "!git status -s"
@@ -8,14 +9,14 @@ git config --global alias.l "!git log --pretty=format:'%C(blue)%h%C(red)%d %C(wh
 if [ -f /etc/os-release ]; then
 	. /etc/os-release
 	case $NAME in
-	"Arch Linux")
+	"$HOME_OS")
 		git config --global user.signingkey "AB2C228810A1F5EE"
-		git config --global user.email "leonardodiber@gmail.com"
+		git config --global user.email "$PERSONAL_EMAIL"
 		git config --global commit.gpgsign true
 		;;
 
-	"Ubuntu")
-		git config --global user.email "leonardo.diniz@dito.com.br"
+	"$WORK_OS")
+		git config --global user.email "$WORK_EMAIL"
 		;;
 	esac
 fi
