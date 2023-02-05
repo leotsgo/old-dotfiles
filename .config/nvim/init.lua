8dff1feaf9a7312b4454e515f7dea4351096b9db
Leonardo Diniz <leonardodiber@gmail.com>
2023-02-05 17:19:49 -0300

    feat(nvim): rounded borders for lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local ok, lazy = pcall(require, "lazy")
 if not ok then
-	return
+  return
 end
 
 require("user.options")
 require("user.keymaps")
 require("user.autocommands")
-lazy.setup("user.plugins")
+lazy.setup("user.plugins", { ui = { border = "rounded" } })
