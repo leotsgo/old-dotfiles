ba24779b562306df6ec24bcbd80300933aef346b
Leonardo Diniz <leonardodiber@gmail.com>
2023-02-23 23:32:23 -0300

    refactor(nvim): moves vim-go to its own plugin file

--- .config/nvim/lua/user/plugins/general.lua
+++ .config/nvim/lua/user/plugins/general.lua
@@ -2,7 +2,6 @@ return {
   "nvim-lua/plenary.nvim",
   "moll/vim-bbye",
   "wellle/targets.vim",
-  "fatih/vim-go",
   "andweeb/presence.nvim",
   "Pocco81/true-zen.nvim",
   "ThePrimeagen/harpoon",
