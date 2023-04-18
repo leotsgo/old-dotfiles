fe2352d948fbf51d6f100b8a1807c55d5e748dc4
Leonardo Diniz <leonardodiber@gmail.com>
2023-04-17 22:15:02 -0300

    fix(nvim): fix deprecated values in nvimtree
    adds colorscheme to harpoon

--- .config/nvim/lua/user/plugins/colorscheme.lua
+++ .config/nvim/lua/user/plugins/colorscheme.lua
@@ -1,23 +1,24 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		-- load the colorscheme here
-		require("catppuccin").setup({
-			integrations = {
-				vim_sneak = true,
-				ts_rainbow = true,
-				noice = true,
-				illuminate = true,
-				hop = true,
-				indent_blankline = {
-					enabled = true,
-					colored_indent_levels = true,
-				},
-			},
-		})
-		vim.cmd([[colorscheme catppuccin-macchiato]])
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,   -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    -- load the colorscheme here
+    require("catppuccin").setup({
+      integrations = {
+        vim_sneak = true,
+        ts_rainbow = true,
+        noice = true,
+        illuminate = true,
+        hop = true,
+        harpoon = true,
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = false,
+        },
+      },
+    })
+    vim.cmd([[colorscheme catppuccin-macchiato]])
+  end,
 }
