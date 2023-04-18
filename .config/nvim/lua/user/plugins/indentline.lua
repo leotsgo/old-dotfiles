65ceb3298ff2d7654a3fe3aa393f96f9f9738c6d
Leonardo Diniz <leonardodiber@gmail.com>
2023-04-17 23:32:41 -0300

    feat(nvim): removes highlight groups from indentline.lua

--- .config/nvim/lua/user/plugins/indentline.lua
+++ .config/nvim/lua/user/plugins/indentline.lua
@@ -1,32 +1,23 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-  config = function()
-    local status_ok, indent_blankline = pcall(require, "indent_blankline")
-    if not status_ok then
-      return
-    end
+	"lukas-reineke/indent-blankline.nvim",
+	config = function()
+		local status_ok, indent_blankline = pcall(require, "indent_blankline")
+		if not status_ok then
+			return
+		end
 
-    vim.opt.list = true
-    vim.opt.listchars:append("space:⋅")
+		vim.opt.list = true
+		vim.opt.listchars:append("space:⋅")
 
-    indent_blankline.setup({
-      use_treesitter = true,
-      show_current_context = true,
-      buftype_exclude = { "terminal", "nofile" },
-      filetype_exclude = {
-        "help",
-        "packer",
-        "NvimTree",
-      },
-      space_char_blankline = " ",
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
-    })
-  end,
+		indent_blankline.setup({
+			use_treesitter = true,
+			show_current_context = true,
+			buftype_exclude = { "terminal", "nofile" },
+			filetype_exclude = {
+				"help",
+				"packer",
+				"NvimTree",
+			},
+		})
+	end,
 }
