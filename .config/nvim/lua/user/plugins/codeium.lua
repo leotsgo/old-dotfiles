0afc3f2d521a7c9c826e4776de0e421e32cfe765
Leonardo Diniz <leonardodiber@gmail.com>
2023-04-17 21:58:56 -0300

    feat(nvim): codeium changes

--- .config/nvim/lua/user/plugins/codeium.lua
+++ .config/nvim/lua/user/plugins/codeium.lua
@@ -1,7 +1,7 @@
 return {
   "Exafunction/codeium.vim",
   config = function()
-    vim.keymap.set("i", "<C-g>", function()
+    vim.keymap.set("i", "<C-Space>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true })
     vim.keymap.set("i", "<c-;>", function()
