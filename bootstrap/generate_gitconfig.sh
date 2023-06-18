46b63eaefdbac075dadf5b512d5c6b5ae2c63fcf
Leonardo Diniz <leonardodiber@gmail.com>
2023-06-18 01:57:00 -0300

    ✨ feat(bootstrap): adds delta support

--- bootstrap/generate_gitconfig.sh
+++ bootstrap/generate_gitconfig.sh
@@ -6,6 +6,11 @@ git config --global user.name "Leonardo Diniz"
 git config --global push.autoSetupRemote true
 git config --global alias.s "!git status -s"
 git config --global alias.l "!git log --pretty=format:'%C(blue)%h%C(red)%d %C(white)%s - %C(green)%cn, %C(cyan)%cr'"
+git config --global core.pager "delta"
+git config --global interactive.diffFilter "delta --color-only"
+git config --global delta.navigate true
+git config --global merge.conflictstyle "diff3"
+git config --global diff.colorMoved "default"
 
 if [ -f /etc/os-release ]; then
 	. /etc/os-release
