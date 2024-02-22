5ff86dcaac5cd7442e6f0bd0b8a8ba5cd0519bba
Leonardo Diniz <leonardodiber@gmail.com>
2024-02-22 19:41:09 -0300

    changes

--- .config/.zsh/aliases.zsh
+++ .config/.zsh/aliases.zsh
@@ -67,6 +67,7 @@ function zr () { zellij run --name "$*" -- zsh -ic "$*";}
 function zrf () { zellij run --name "$*" --floating -- zsh -ic "$*";}
 function ze () { zellij edit "$*";}
 function zef () { zellij edit --floating "$*";}
+alias pvpn='protonvpn-cli'
 
 # yadm
 function yl() {
