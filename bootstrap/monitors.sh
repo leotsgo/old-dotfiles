1d62fb9bfc49c8a48a08b0056e77b2d8513ddcf3
Leonardo Diniz <leonardodiber@gmail.com>
2024-02-28 18:43:23 -0300

    script

---
+++ bootstrap/monitors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+input=$(hyprctl monitors)
+
+awk '/Monitor/ {match($0, /Monitor[[:space:]]*([^[:space:]]+)/, arr); print arr[1]}' <<< "$input" |
+while read -r monitor_name; do
+    if [ "$monitor_name" == "HDMI-A-1" ]; then
+        hyprctl keyword monitor "eDP-1, disable"
+    else
+        hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
+    fi
+done
+
+
+
+
+
