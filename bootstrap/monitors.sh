34c146c7436ab4df17c16bf5c3b2cb2524e1ad89
Leonardo Diniz <leonardodiber@gmail.com>
2024-04-07 18:53:41 -0300

    🐛 fix(hyprland): fixing hyprland monitors

--- bootstrap/monitors.sh
+++ bootstrap/monitors.sh
@@ -8,6 +8,7 @@ while read -r monitor_name; do
     if [ "$monitor_name" == "HDMI-A-1" ]; then
         hyprctl keyword monitor "eDP-1, disable"
     else
+        # hyprctl keyword monitor "eDP-1, enable"
         hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
     fi
 done
